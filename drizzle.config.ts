e3e44152e733d5c3d580d071739450829707196f
zerby <zerby.programming@proton.me>
2026-01-02 00:20:49 +0100

    fix(drizzle): 🔧 fix broken drizzle config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "drizzle-kit"
-import { env } from "./src/env"
+import { z } from "zod"
+
+const DATABASE_URL = z.string().parse(process.env.DATABASE_URL)
 
 export default defineConfig({
     dialect: "postgresql",
     schema: "./src/db/schema.ts",
     out: "./drizzle",
     dbCredentials: {
-        url: env.DATABASE_URL,
+        url: DATABASE_URL,
     },
 })
